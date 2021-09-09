453c48880b72914ec998c1cefa96c544769b16e5
NataliiaKudaieva <81326193+NataliiaKudaieva@users.noreply.github.com>
2021-09-09 16:55:54 +0300

    arrays is finished

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-var letters = ['a', 'b', 'c'];
-var lettersAndNumbers = letters.push (1,2,3);
-console.log(letters);
+// var letters = ['a', 'b', 'c'];
+// var lettersAndNumbers = letters.push (1,2,3);
+// console.log(letters);
 
-var numbers = [1,2,3];
-var reverseN = numbers.reverse();
-console.log(numbers);
+// var numbers = [1,2,3];
+// var reverseN = numbers.reverse();
+// console.log(numbers);
 
-var someNumbers = [1,2,3];
-var toFirstNumbers = someNumbers.unshift(4,5,6);
-console.log(someNumbers);
+// var someNumbers = [1,2,3];
+// var toFirstNumbers = someNumbers.unshift(4,5,6);
+// console.log(someNumbers);
 
-var numberArray = [1,2,3,4,5];
-var deleted = numberArray.slice(0,3);
-console.log (deleted);
+// var numberArray = [1,2,3,4,5];
+// var deleted = numberArray.slice(0,3);
+// console.log (deleted);
 
-var screen = ['js', 'css', 'jq'];
-console.log(screen[0]);
+// var screen = ['js', 'css', 'jq'];
+// console.log(screen[0]);
 
+function fromPairs(array) {
+    return Object.fromEntries(array)
+}
+
+const array = [['a', 1], ['b', 2]];
+console.log(fromPairs(array)) // { 'a': 1, 'b': 2 }
